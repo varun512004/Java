23677a9e844d2ec412961be5cd9f68475e9bbc2d
Varun Tiwari <varuali143@gmail.com>
2025-11-07 14:37:50 +0530

    Initial Commit

---
+++ Hello.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class Hello {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.print("Enter the value of n: ");
+        // int n = sc.nextInt();
+        // if (n < 0){
+        //     System.out.println(n + " is Negative");
+        // } else {
+        //     System.out.println(n + " is Positive");
+        // }
+
+        // System.out.print("Enter your temperature: ");
+        // double temp = sc.nextDouble();
+        // if(temp > 100) {
+        //     System.out.println("You have FEVER.");
+        // } else {
+        //     System.out.println("You don't have FEVER.");
+        // }
+
+        // System.out.print("Enter the value of n: ");
+        // int n = sc.nextInt();
+        // int sum = 0;
+        // int counter = 1;
+        // if(n < 1) {
+        //     System.out.println("You entered negative number.");
+        // } else {
+        //     while (counter <= n ) {
+        //         sum += counter;
+        //         counter++;
+        //     }
+        // System.out.println("Sum of N natural numbers is: " + sum);
+        // }
+
+        // System.out.print("Enter the value of n: ");
+        // int n = sc.nextInt();
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = 0; j < n; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();  
+        // }
+
+        // System.out.print("Enter the value of n: ");
+        // int n = sc.nextInt();
+        // int reverse = 0;
+        // while(n > 0){
+        //     int lastDigit = n % 10;
+        //     n = n / 10;
+        //     reverse = (reverse * 10) + lastDigit;
+        // }
+        // System.out.print(reverse);
+
+        sc.close();
+    }
+}
