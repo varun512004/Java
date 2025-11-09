7f15ab5dc54efc0a2708d2fc633532de48c8cd3c
Varun-T20 <varuntiwari5104@gmail.com>
2025-11-09 19:38:27 +0530

    9/11/25

--- Hello.java
+++ Hello.java
@@ -3,6 +3,98 @@ public class Hello {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
+        // int a = 5;
+        // a = 10;
+        // System.out.print("The value of a: " + a);
+        
+        // int $ = 24;
+        // System.out.println($);
+
+        // System.out.print("Enter the side of the square:");
+        // float side = sc.nextFloat();
+        // float area = side * side;
+        // System.out.println("The area of the square is:" + area);
+
+        // System.out.print("Enter the value of a:");
+        // int a = sc.nextInt();
+        // System.out.print("Enter the value of b:");
+        // int b = sc.nextInt();
+        // System.out.println("The Sum of a & b :" + (a + b));
+
+        // double pi = 3.14;
+        // System.out.print("Enter the radius of the circle: ");
+        // double radius = sc.nextDouble();
+        // System.out.println("The area of the circle is: " + (pi * radius * radius));
+
+        // System.out.print("Enter the value of a: ");
+        // int a = sc.nextInt();
+        // System.out.print("Enter the value of b: ");
+        // int b = sc.nextInt();
+        // System.out.print("Enter the value of c: ");
+        // int c = sc.nextInt();
+        // double sum = a + b + c;
+        // double gst =  sum * 0.18;
+        // System.out.println("The average of a, b and c is: " + sum / 3 );
+        // System.out.println("The total price after GST is: " + (sum + gst));
+
+        // int x = 9, y = 12;
+        // int a = 2, b = 4, c = 6;
+        // int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);
+        // System.out.println(exp);
+
+        // System.out.print("Enter the value of A: ");
+        // int a = sc.nextInt();
+        // System.out.print("Enter the value of B: ");
+        // int b = sc.nextInt();
+        // System.out.print("Enter the value of C: ");
+        // int c = sc.nextInt();
+        // if(a >= b && a > c) {
+        //     System.out.println("A: " + a + " is the greatest number.");
+        // } else if (b > c) {
+        //     System.out.println("B: " + b + " is the greatest number.");
+        // } else{
+        //     System.out.println("C: " + c + " is the greatest number.");
+        // }
+
+        // System.out.print("Enter the value of n: ");
+        // int n = sc.nextInt();
+        // System.out.print((n % 2 == 0) ? n +  " is Even." : n + " is Odd.");
+
+        // System.out.print("Week no :");
+        // int n = sc.nextInt();
+        // switch(n) {
+        //     case 1:
+        //     System.out.println("Monday");
+        //     break;
+
+        //     case 2:
+        //     System.out.println("Tuesday");
+        //     break;
+
+        //     case 3:
+        //     System.out.println("Wednesday");
+        //     break;
+
+        //     case 4:
+        //     System.out.println("Thursday");
+        //     break;
+
+        //     case 5:
+        //     System.out.println("Friday");
+        //     break;
+
+        //     case 6:
+        //     System.out.println("Saturday");
+        //     break;
+
+        //     case 7:
+        //     System.out.println("Sunday");
+        //     break;
+
+        //     default:
+        //     System.out.println("Invalid week number.");
+        // }
+
         // System.out.print("Enter the value of n: ");
         // int n = sc.nextInt();
         // if (n < 0){
