a6b647501f679e1479811f168ad2d92e96516db6
Varun-T20 <varuntiwari5104@gmail.com>
2026-06-19 02:45:20 +0530

    Pattern

--- Pattern.java
+++ Pattern.java
@@ -2,11 +2,49 @@ import java.util.Scanner;
 public class Pattern {
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the number of rows: ");
+        // int n = sc.nextInt();
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.print("Enter the number of rows: ");
+        // int n = sc.nextInt();
+        // for (int i = 1; i <= n; i++) {
+        //     for(int j = 1; j <= i; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.print("Enter the number of rows: ");
+        // int n = sc.nextInt();
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = i; j <= n; j++){  //  j = 1; j <= (n-i+1)
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.print("Enter the number of rows: ");
+        // int n = sc.nextInt();
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+
         System.out.print("Enter the number of rows: ");
         int n = sc.nextInt();
+        char ch = 'A';
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                System.out.print("* ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+                ch++;
             }
             System.out.println();
         }
